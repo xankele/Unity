085474f31d367c7bb9bad22be4854b915f706ffd
Ania <annamikulak99@gmail.com>
2021-10-18 18:24:24 +0200

    added 3-zadanie2

---
+++ New Unity Project 3/Assets/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 10f;
+    Rigidbody rb;
+    public float start = 0;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if(start == 0)
+        {
+            if(transform.position.x > 10f)
+            {
+                start = 1;
+            }
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            if(transform.position.x < 0f)
+            {
+                start = 0;
+            }
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
+
