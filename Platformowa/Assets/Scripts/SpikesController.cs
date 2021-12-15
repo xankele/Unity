82e2ce14fb77b980e0fde3b3cae960784be71939
Ania <annamikulak99@gmail.com>
2021-12-15 01:44:02 +0100

    ended game

--- Platformowa/Assets/Scripts/SpikesController.cs
+++ Platformowa/Assets/Scripts/SpikesController.cs
@@ -11,7 +11,7 @@ public class SpikesController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == "Spike")
         {
             Player.transform.position = new Vector2(50.0f, 5.0f);
         }
